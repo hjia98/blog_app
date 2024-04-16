89df62c16bfb6ad027dd99dbc43ee4b72fbf0d13
Gia <hoanggia1611@gmail.com>
2024-04-16 17:14:32 +0700

    feat: add form, validator

--- lib/features/auth/presentation/widgets/auth_field.dart
+++ lib/features/auth/presentation/widgets/auth_field.dart
@@ -2,15 +2,31 @@ import 'package:flutter/material.dart';
 
 class AuthField extends StatelessWidget {
   final String hintText;
-  const AuthField({super.key, required this.hintText});
+  final TextEditingController controller;
+  final bool isObscureText;
+
+  const AuthField({
+    super.key,
+    required this.hintText,
+    required this.controller,
+    this.isObscureText = false,
+  });
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      controller: controller,
       decoration: InputDecoration(
         hintText: hintText,
         border: const OutlineInputBorder(),
       ),
+      validator: (value) {
+        if (value!.isEmpty) {
+          return "$hintText is required";
+        }
+        return null;
+      },
+      obscureText: isObscureText,
     );
   }
 }
