8bce0881356effe8b3be001e8e78a8faf926f0eb
Gia <hoanggia1611@gmail.com>
2024-04-17 15:51:42 +0700

    feat: add supabase, auth_repository, auth_remote_data_source, core/error/, fpdart

---
+++ lib/core/secrets/app_secrets.dart
@@ -0,0 +1,5 @@
+class AppSecrets {
+  static const supabaseUrl = 'https://yahvjtjtgczcnhfnixxw.supabase.co';
+  static const supabaseAnonKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlhaHZqdGp0Z2N6Y25oZm5peHh3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTMzMzg1MDIsImV4cCI6MjAyODkxNDUwMn0.yPTAEARsO1UA0cSAAPHPxzXXXGmpD5bT7TcrnkIDEss';
+}
