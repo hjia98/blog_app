07b98601ba421603fb226aaeaa3f62d0180ce28e
Gia <hoanggia1611@gmail.com>
2024-04-22 14:15:25 +0700

    feat: add entities: user, model: user, fix type error

---
+++ lib/features/auth/data/models/user_model.dart
@@ -0,0 +1,19 @@
+import 'package:blog_app/features/auth/domain/entities/user.dart';
+
+class UserModel extends User {
+  UserModel({
+    required super.id,
+    required super.email,
+    required super.name,
+  });
+
+  /// Convert dữ liệu từ Map<String, dynamic> sang UserModel để tái sử dụng trong AuthRemoteDataSourceImpl
+  /// Mà không cần phải chuyển đổi response từ SupabaseClient (Json) sang UserModel mỗi lần sử dụng
+  factory UserModel.fromJson(Map<String, dynamic> map) {
+    return UserModel(
+      id: map['id'] ?? '',
+      email: map['email'] ?? '',
+      name: map['name'] ?? '',
+    );
+  }
+}
